1f1c156539c0256dcbaaa1911be44474ec3cac8a
mulukimwangeka <mwangekaruth@gmail.com>
2023-02-10 14:45:32 +0300

    branding page setup

---
+++ src/Components/Branding.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function Branding() {
+    return (
+        <div>
+        <Navbar />
+        <h1>Branding</h1>
+        <Footer />
+        </div>
+    );
+    }
+
+export default Branding;
+
+
