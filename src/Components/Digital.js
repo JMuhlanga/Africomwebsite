bbc487db0030c5a30f8e4b3eb82f31108298b96c
mulukimwangeka <mwangekaruth@gmail.com>
2023-02-10 14:51:07 +0300

    Digital page setup

---
+++ src/Components/Digital.js
@@ -0,0 +1,19 @@
+//setup digital page
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+class Digital extends Component {
+    render() {
+        return (
+            <div>
+            <Navbar />
+            <h1>Digital</h1>
+            <Footer />
+            </div>
+        );
+    }
+}
+
+export default Digital;
+
