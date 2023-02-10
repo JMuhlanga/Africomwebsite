7e963251ee56c637cbce58c7fe4b3d4933dfbfcd
mulukimwangeka <mwangekaruth@gmail.com>
2023-02-10 14:55:48 +0300

    JOBPAGE SETUP

---
+++ src/Components/Jobpages.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+class Jobpages extends Component {
+    render() {
+        return (
+            <div>
+            <h1>Jobpages</h1>
+            </div>
+        );
+    }
+}
+
+export default Jobpages;
