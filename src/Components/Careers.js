20d7ea07af09628920e021f9f893052df707cf90
mulukimwangeka <mwangekaruth@gmail.com>
2023-02-10 14:50:03 +0300

    contact page setup

--- src/Components/Careers.js
+++ src/Components/Careers.js
@@ -1,4 +1,3 @@
-//setup career js
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
---
+++ src/Components/Contact.js
@@ -0,0 +1,19 @@
+//setup contact js
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+            <Navbar />
+            <h1>Contact</h1>
+            <Footer />
+            </div>
+        );
+    }
+}
+
+export default Contact;
+
