c7c26faf94981973a443a8847c530895cdde8b6b
mulukimwangeka <mwangekaruth@gmail.com>
2023-02-10 14:47:34 +0300

    careers setup

---
+++ src/Components/Careers.js
@@ -0,0 +1,16 @@
+//setup career js
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+function Careers() {
+    return (
+        <div>
+        <Navbar />
+        <h1>Careers</h1>
+        <Footer />
+        </div>
+    );
+    }
+
+export default Careers;
