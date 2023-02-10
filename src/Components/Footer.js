a9e5bdbcb9b602341d492399a9704c82c7a49b66
mulukimwangeka <mwangekaruth@gmail.com>
2023-02-10 14:52:30 +0300

    Footer page setup

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+//create a footer page
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <h1>Footer</h1>
+            </div>
+        );
+    }
+}
+
+export default Footer;
+    
+
