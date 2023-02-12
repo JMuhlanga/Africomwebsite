e5d5fd2f063c6c8262b942a6834e4d91f6f755ed
mulukimwangeka <mwangekaruth@gmail.com>
2023-02-12 14:29:57 +0300

    About us div added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,9 +8,11 @@ function Home() {
         <div className="homepage">
         <div className="homepage__picture">
             <img src={require('../Images/HOME.png')} alt="me"  />
-            {/* add a button inside the img */}
-            <button className="homepage__button">Contact Me</button>
-            
+            {/* add arrow facing button to proceed to the next div */}
+            <button className="homepage__button">down icon</button>  
+        </div>
+        <div className="homepage__about">
+            <h1 className="homepage__about__title">About Us</h1>
         </div>
         </div>
         
